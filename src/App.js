99ec29670b261ae8e197e892dd2a30b20cf06bd1
Travis from Discord <sharryboy88@gmail.com>
2025-03-04 20:23:05 +0100

    Aktualisierte Version – kompletter Upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route } from 'react-router-dom'; // Nur diese Zeile behalten
 import './App.scss';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home';
 import Layout from './components/Layout/Layout';
@@ -9,6 +8,7 @@ import Rules from './components/Rules/Rules';
 import About from './components/About/About';
 import Game from './components/Game/Game';
 import CoOpGame from './components/Coop/CoOpGame.js'; // Importiere den Co-Op-Modus
+
 class App extends Component {
     render() {
         return (
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,14 +7,12 @@ import Container from '../Container/Container';
 const About = () => (
     <Container>
         <div className={classes.about}>
-            <h2 className={classes.heading__primary}>About Us</h2>
-            <div className={classes.about__presentation}>We are Stranweb, a team full of passionate young developers</div>
+            <h2 className={classes.heading__primary}>About Me</h2>
+            <div className={classes.about__presentation}>I am sharryboy, a guy who likes Kaiji and wanted to try coding.</div>
             <div className={classes.about__circles}>
-                <a href='https://facebook.com' target="_blank" rel="noopener noreferrer" className={classes.about__circle}>Facebook</a>
-                <a href='https://discord.gg/9W9kCNA' target="_blank" rel="noopener noreferrer" className={classes.about__circle}>Discord</a>
-                <a href='https://stranweb.com' target="_blank" rel="noopener noreferrer" className={classes.about__circle}>Website</a>
+                <a href='https://discord.gg/Jaqueswebster' target="_blank" rel="noopener noreferrer" className={classes.about__circle}>Discord</a>
+                <a href='https://github.com/sharryboy88/e-card-v2' target="_blank" rel="noopener noreferrer" className={classes.about__circle}>Website</a>
             </div>
-            <p className={classes.about__email}>Email: stranweb@gmail.com </p>
         
         </div>
     </Container>
